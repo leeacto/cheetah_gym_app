ad91824444faad50f2145c746ce409783ab7dccb
Nicholas <nicholas.lee.3@gmail.com>
2013-02-15 19:53:19 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheetahGymApp::Application.config.secret_token = '636ebba878aa35de81a1952d14129dc24f42362ac484b4b7b36857e7366575d68928c7c418aaf986892f0e723e09620f285805e065927938a980219119872f0d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CheetahGymApp::Application.config.session_store :cookie_store, key: '_cheetah_gym_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CheetahGymApp::Application.config.session_store :active_record_store
