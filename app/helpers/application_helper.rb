c4bf5f36080221bd2ff7cb400bcce2d319246d0d
Nicholas <nicholas.lee.3@gmail.com>
2013-02-15 20:19:51 -0600

    Done with static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+	# Return a title on a per-page basis.
+	def title
+		base_title = "Cheetah Crossfit" 
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end 
+	end
+
+	def logo
+		image_tag("cflogo.gif", :alt => "Sample App", :class => "round") 
+	end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,33 +1,44 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'home'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "Cheetah Crossfit | Home")
+   end
   end
 
   describe "GET 'contact'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", 
+                                    :content => "Cheetah Crossfit | Contact")
+   end
   end
 
   describe "GET 'about'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'about'
       response.should be_success
     end
-  end
 
-  describe "GET 'help'" do
-    it "returns http success" do
-      get 'help'
-      response.should be_success
-    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", 
+                                    :content => "Cheetah Crossfit | About")
+   end
   end
-
 end
