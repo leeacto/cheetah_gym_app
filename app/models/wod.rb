72443ecef50c39940cd7bb829a4cc752deacf526
Nicholas <nicholas.lee.3@gmail.com>
2013-10-18 07:39:42 -0500

    turn off paginate on wod index

--- app/models/wod.rb
+++ app/models/wod.rb
@@ -34,7 +34,7 @@ class Wod < ActiveRecord::Base
   
   def self.search(search)
     if search
-      where 'name LIKE ? or desc LIKE ? or seq LIKE ?', "%#{search}%","%#{search}%","%#{search}%"
+      where('name LIKE ? or desc LIKE ? or seq LIKE ?', "%#{search}%","%#{search}%","%#{search}%")
     else
       scoped
     end
