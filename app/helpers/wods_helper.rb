94578aec24306c8b56a629d39aeec9967aac6f01
Nicholas <nicholas.lee.3@gmail.com>
2013-10-24 12:08:11 -0500

    tesing helper

--- app/helpers/wods_helper.rb
+++ app/helpers/wods_helper.rb
@@ -20,16 +20,16 @@ module WodsHelper
       return '' if total_recd == nil
       avg_recd = total_recd.to_f/wod.results.count
     end
-
-    if wod.wod_type == "Time"
-      Time.local(1999,1,1,0, (avg_recd/wod.baserep).to_i, (avg_recd%wod.baserep).to_i).strftime "%M:%S"
-    else
-      formatted = "#{(avg_recd/wod.baserep).to_i}"
-      if avg_recd % wod.baserep > 0
-        +
-       formatted += "+ #{avg_recd % wod.baserep}"
-      end
-      formatted
-    end
+    "TEST"
+    # if wod.wod_type == "Time"
+    #   Time.local(1999,1,1,0, (avg_recd/wod.baserep).to_i, (avg_recd%wod.baserep).to_i).strftime "%M:%S"
+    # else
+    #   formatted = "#{(avg_recd/wod.baserep).to_i}"
+    #   if avg_recd % wod.baserep > 0
+    #     +
+    #    formatted += "+ #{avg_recd % wod.baserep}"
+    #   end
+    #   formatted
+    # end
   end
 end
