f505e8b2b59602f6780439e4ffee8596a18b9123
Nicholas <nicholas.lee.3@gmail.com>
2013-10-24 12:45:31 -0500

    try full

--- app/helpers/wods_helper.rb
+++ app/helpers/wods_helper.rb
@@ -25,12 +25,12 @@ module WodsHelper
       Time.local(1999,1,1,0, (avg_recd/wod.baserep).to_i, (avg_recd%wod.baserep).to_i).strftime "%M:%S"
     else
       formatted = "#{(avg_recd/wod.baserep).to_i}"
-      (avg_recd/wod.baserep).to_i
-    #   if avg_recd % wod.baserep > 0
-    #     +
-    #    formatted += "+ #{avg_recd % wod.baserep}"
-    #   end
-    #   formatted
+      
+      if avg_recd % wod.baserep > 0
+       
+       formatted += "+ #{avg_recd % wod.baserep}"
+      end
+      formatted
     end
   end
 end
