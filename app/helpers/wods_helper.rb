10dc56a0285b70fd44f68695ee0db143874eedfd
Nicholas <nicholas.lee.3@gmail.com>
2013-10-24 12:34:36 -0500

    testing rounds

--- app/helpers/wods_helper.rb
+++ app/helpers/wods_helper.rb
@@ -25,7 +25,7 @@ module WodsHelper
       Time.local(1999,1,1,0, (avg_recd/wod.baserep).to_i, (avg_recd%wod.baserep).to_i).strftime "%M:%S"
     else
       formatted = "#{(avg_recd/wod.baserep).to_i}"
-      avg_recd
+      avg_recd/wod.baserep
     #   if avg_recd % wod.baserep > 0
     #     +
     #    formatted += "+ #{avg_recd % wod.baserep}"
