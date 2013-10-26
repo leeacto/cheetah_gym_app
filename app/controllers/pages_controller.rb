8ef02c48aac3d8f29c08a2b9c76efb1e01dbc91b
Nicholas <nicholas.lee.3@gmail.com>
2013-10-26 17:48:24 -0500

    formatted root buttons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
-    @daywods = Daywod.order("performed DESC").paginate(:page => params[:page])
+    @daywods = Daywod.where(:personal => false).order("performed DESC").paginate(:page => params[:page])
   end
 
   def contact
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     @user.name = @user.name.titleize
+    @user.email = @user.email.downcase
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to Cheetah Crossfit!"
