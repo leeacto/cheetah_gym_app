a7bb05a6f3555ec242a18b05dd7b0bcd0daaea77
Nicholas <nicholas.lee.3@gmail.com>
2013-10-29 09:28:54 -0500

    validations fixed

--- spec/models/daywod_spec.rb
+++ spec/models/daywod_spec.rb
@@ -6,10 +6,15 @@ describe Daywod do
     @attr = { :performed => "02/01/2013" }
   end
 
-  it "should create a new instance given valid attributes" do 
-    @wod.daywods.create!(@attr)
-  end
+  describe 'create' do
+    it "should create a new instance given valid attributes" do 
+      expect{@wod.daywods.create!(@attr)}.to change(Daywod, :count).by(1)
+    end
 
+    it "does not create a new daywod given invalid attributes" do
+      expect{@wod.daywods.create!()}.to raise_error
+    end
+  end
   describe "WOD Associations" do
     
     before(:each) do
@@ -23,6 +28,21 @@ describe Daywod do
     it "should have the right associated wod" do
       @daywod.wod_id.should == @wod.id 
       @daywod.wod.should == @wod 
+    end
+  end
+
+  describe "Validations" do
+    before(:each) do
+      @other_wod = FactoryGirl.create(:wod, :name => "Other")
+      @wod.daywods.create!(@attr)
+    end
+
+    it "creates another daywod for a separate wod" do
+      expect{@other_wod.daywods.create!(@attr)}.to change(Daywod, :count).by(1)
+    end
+
+    it "does not create a daywod for the same wod on same day" do
+      expect{@wod.daywods.create!(@attr)}.to raise_error
     end
   end
 end
