5fea3021586b7cc6c901ffda72709d52a1b44be4
Nicholas Lee <leeacto@Nicholass-MacBook-Pro-2.local>
2013-11-25 19:06:35 -0600

    edit bio

--- app/controllers/bios_controller.rb
+++ app/controllers/bios_controller.rb
@@ -1,5 +1,14 @@
 class BiosController < ApplicationController
 
+  def create
+    params[:height] = params[:bio][:feet]*12 + params[:bio][:inches]
+    params[:bio].delete(:feet)
+    params[:bio].delete(:inches)
+    params[:bio][:user_id] = current_user.id
+    @bio = Bio.create(params[:bio])
+    redirect_to @bio.user
+  end
+
   def edit
     @bio = Bio.find(params[:id])
   end
--- app/models/bio.rb
+++ app/models/bio.rb
@@ -2,5 +2,11 @@ class Bio < ActiveRecord::Base
   attr_accessible :experience, :fav, :height, :unfav, :user_id, :weight
   belongs_to :user
 
+  def feet
+    height.to_i / 12
+  end
 
+  def inches
+    height.to_i % 12
+  end
 end
